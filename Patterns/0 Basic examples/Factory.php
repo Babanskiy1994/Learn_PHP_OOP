e5a22f7d4a1450cb3986f2278283f5a2c29e36d7
Aleksey Babanskiy <a.babanskiy@crpo.su>
2023-02-07 21:45:38 +0300

    Дополнение

---
+++ Patterns/0 Basic examples/Factory.php
@@ -0,0 +1,56 @@
+<?php
+
+class carFactory {
+
+    public function __construct() {
+        // ... //
+    }
+
+    public static function build ($type = '') {
+
+        if($type == '') {
+            throw new Exception('Invalid Car Type.');
+        } else {
+
+            $className = 'car_'.ucfirst($type);
+
+            // проверка существования класса
+            if(class_exists($className)) {
+                return new $className();
+            } else {
+                throw new Exception('Car type not found.');
+            }
+        }
+    }
+}
+
+class car_Sedan {
+
+    public function __construct() {
+        echo "Creating Sedan" . PHP_EOL;
+    }
+
+}
+
+class car_Suv {
+
+    public function __construct() {
+        echo "Creating SUV" . PHP_EOL;
+    }
+
+}
+
+// Создаём новый Sedan
+$sedan = carFactory::build('sedan');
+
+// Создаём новый SUV
+$suv = carFactory::build('suv');
+
+
+
+/* 
+Применимость:
+
+    Есть главный класс который реализовывает механники работы с каким-то классом с помощбю объекта (но сам объект не пораждает, а заставлет это делать дочерние классы). 
+Механника у всех дочерних классов будет одинаковая, но разные объекты.
+*/
